254b42da428a6b56010f64346de8241f40fe9953
RonaldQE <r24.espinoza@gmail.com>
2022-08-01 20:33:29 -0400

    barra  de navegacion y dependencias

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <div className="App">
+            <h1>Matematica financiera</h1>
+        </div>
+    )
+}
+export default Home;
---
+++ src/pages/Simulation.js
@@ -0,0 +1,8 @@
+function Simulation(){
+return (
+    <div className="App">
+    <h1>Simular Credito</h1>
+  </div>
+)
+}
+export default Simulation;
